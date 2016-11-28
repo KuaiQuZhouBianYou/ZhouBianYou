f682651b77e7c64374facb65c278e501d382d974
Gzy <1281785095@qq.com>
2016-11-28 17:21:24 +0800

    Gzy xutils gson

--- app/src/main/java/com/phone1000/wanttozhoubianyou/MyApp.java
+++ app/src/main/java/com/phone1000/wanttozhoubianyou/MyApp.java
@@ -2,6 +2,8 @@ package com.phone1000.wanttozhoubianyou;
 
 import android.app.Application;
 
+import org.xutils.x;
+
 /**
  * Created by 落叶 on 2016-11-28.
  */
@@ -11,5 +13,7 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        x.Ext.init(this);
+        x.isDebug();
     }
 }
