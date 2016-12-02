9a902792e9644738f41f2e5de3f76fd6dfa82d33
jim4610zhao <1137358557@qq.com>
2016-12-02 15:40:08 +0800

    around sconedpager has bean finished

--- app/src/main/java/com/phone1000/wanttozhoubianyou/activity/around/PictureActivity.java
+++ app/src/main/java/com/phone1000/wanttozhoubianyou/activity/around/PictureActivity.java
@@ -1,12 +1,19 @@
 package com.phone1000.wanttozhoubianyou.activity.around;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.gson.Gson;
 import com.phone1000.wanttozhoubianyou.R;
 import com.phone1000.wanttozhoubianyou.adapter.around.ViewPagerAdapter;
+import com.phone1000.wanttozhoubianyou.model.around.Scienic;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 /**
  * Created by 落叶 on 2016-12-02.
@@ -16,19 +23,51 @@ public class PictureActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private ViewPagerAdapter adapter;
+    private String url;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_picture);
+
+        Intent intent = getIntent();
+        url = intent.getStringExtra("url");
+
         initView();
         setupData();
 
     }
 
     private void setupData() {
+        RequestParams requestParams = new RequestParams(url);
+        x.http().get(requestParams, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Gson gson = new Gson();
+                Scienic scienic = gson.fromJson(result, Scienic.class);
 
+
+
+
+
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
 
 
     }
